7c79ce0e1754a440e7a7ab9261859c9a848ffb7f
Shrawani Medankar <shrawanimedankar@gmail.com>
2026-02-15 12:03:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# 📝 Todo List App | React
+
+A simple and responsive Todo List application built with React.js.
+It allows users to add, delete, and mark todos as done. Designed with a clean UI using CSS Flexbox and supports mobile responsiveness.
+
+## 🌐 Live Demo:
+
+https://random-joke-generator-vlnc.onrender.com/
+
+---
+
+## Features
+
+- Add new todos
+- Mark individual todos as done/undone
+- Mark all todos as done
+- Delete individual todos
+- Responsive design for desktop and mobile
+- Unique IDs for todos using uuid
+
+## Technologies Used
+
+- React.js – Frontend library
+- CSS Flexbox – Styling and layout
+- UUID – Unique identifiers for todos
+
+## How It Works
+
+- Enter a task in the input field and click Add.
+- Use the checkbox to mark individual tasks as done.
+- Click Mark All as Done to complete all tasks.
+- Click the trash icon to delete a task.
---
+++ src/TodoList.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export default function TodoList() {
+  let [todos, setTodos] = useState([
+    { task: "Eat", id: uuidv4(), isDone: false },
+  ]);
+  let [newTodo, setnewTodo] = useState("");
+
+  let addTodo = () => {
+    setTodos((prevTodo) => {
+      return [...prevTodo, { task: newTodo, id: uuidv4(), isDone: false }];
+    });
+    setnewTodo("");
+  };
+
+  let updateTodoValue = (event) => {
+    setnewTodo(event.target.value);
+  };
+
+  let deleteTodo = (id) => {
+    setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+  };
+
+  let markAsDoneAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      }),
+    );
+  };
+
+  let markAsDoneOne = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  };
+
+  return (
+    <div className="card">
+      <h1>Todo List </h1>
+      <div className="input-row">
+        <input
+          placeholder="Add a Todo"
+          value={newTodo}
+          onChange={updateTodoValue}
+        />
+        <button className="add" onClick={addTodo}>
+          Add
+        </button>
+      </div>
+      <h4>My Todos</h4>
+      <ol>
+        {todos.map((todo) => (
+          <li className="todo-item" key={todo.id}>
+            <div className="left">
+              <input
+                type="checkbox"
+                checked={todo.isDone}
+                onChange={() => markAsDoneOne(todo.id)}
+              />
+              <span className="number" />
+              <span className={todo.isDone ? "done" : ""}>{todo.task}</span>
+            </div>
+            <button className="delete" onClick={() => deleteTodo(todo.id)}>
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </li>
+        ))}
+      </ol>
+      <br />
+      <button className="marked" onClick={markAsDoneAll}>
+        Mark All as Done{" "}
+      </button>
+    </div>
+  );
+}
